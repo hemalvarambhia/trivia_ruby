1b2b439a8efe470dc50ef79e7db856713dc23fb6
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-09-24 20:50:29 +0100

    Refactor - improved test set up and description of the expected behaviour.

--- spec/ugly_trivia/game_spec.rb
+++ spec/ugly_trivia/game_spec.rb
@@ -2,24 +2,26 @@ require 'spec_helper'
 require 'ugly_trivia/game'
 describe UglyTrivia::Game do
   let(:game) { UglyTrivia::Game.new }
-  
-  it 'has 50 pop questions' do
-    expected = Array.new(50) { |number| "Pop Question #{number}"}
-    expect(game.pop_questions).to eq expected
-  end
 
-  it 'has 50 science questions' do
-    expected = Array.new(50) { |number| "Science Question #{number}" }
-    expect(game.science_questions).to eq expected
-  end
+  describe 'game set up' do
+    it 'consists of 50 pop questions' do
+      expected = Array.new(50) { |number| "Pop Question #{number}"}
+      expect(game.pop_questions).to eq expected
+    end
 
-  it 'has 50 sports questions' do
-    expected = Array.new(50) { |number| "Sports Question #{number}" }
-    expect(game.sports_questions).to eq expected
-  end
+    it 'consists of 50 science questions' do
+      expected = Array.new(50) { |number| "Science Question #{number}" }
+      expect(game.science_questions).to eq expected
+    end
 
-  it 'has 50 rock questions' do
-    expected = Array.new(50) { |number| "Rock Question #{number}" }
-    expect(game.rock_questions).to eq expected
+    it 'consists of 50 sports questions' do
+      expected = Array.new(50) { |number| "Sports Question #{number}" }
+      expect(game.sports_questions).to eq expected
+    end
+
+    it 'consists of 50 rock questions' do
+      expected = Array.new(50) { |number| "Rock Question #{number}" }
+      expect(game.rock_questions).to eq expected
+    end
   end
 end
