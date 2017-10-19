104296bc334ebd390b011c4b93be9c48ec747b89
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-10-19 21:55:31 +0100

    Refactor - replaced state variables with intent-revealing methods.

--- lib/ugly_trivia/game.rb
+++ lib/ugly_trivia/game.rb
@@ -74,21 +74,21 @@ module UglyTrivia
     end
 
     def roll(roll)
-      puts "#{@players[@current_player]} is the current player"
+      puts "#{name_of(current_player)} is the current player"
       puts "They have rolled a #{roll}"
 
-      if @in_penalty_box[@current_player]
+      if is_in_penalty_box?(current_player)
         if roll.even?
-          puts "#{@players[@current_player]} is not getting out of the penalty box"
+          puts "#{name_of(current_player)} is not getting out of the penalty box"
           @is_getting_out_of_penalty_box = false
           return
         else
           @is_getting_out_of_penalty_box = true
-          puts "#{@players[@current_player]} is getting out of the penalty box"
+          puts "#{name_of(current_player)} is getting out of the penalty box"
         end
       end
 
-      move(player: @current_player, roll: roll)
+      move(player: current_player, roll: roll)
       puts "The category is #{current_category}"
       ask_question
     end
@@ -100,31 +100,31 @@ module UglyTrivia
     end
 
     def current_category
-      @categories.fetch(@places[@current_player], 'Rock')
+      @categories.fetch(@places[current_player], 'Rock')
     end
 
   public
 
     def was_correctly_answered
-      if @in_penalty_box[@current_player]
+      if is_in_penalty_box?(current_player)
         if @is_getting_out_of_penalty_box
           puts 'Answer was correct!!!!'
-          award_gold_coin_to(@current_player)
+          award_gold_coin_to(current_player)
         end
       else
         puts "Answer was corrent!!!!"
-        award_gold_coin_to(@current_player)
+        award_gold_coin_to(current_player)
       end
 
       winner = did_player_win
-      next_players_turn          
+      next_players_turn      
       winner
     end
 
     def wrong_answer
       puts 'Question was incorrectly answered'
-      puts "#{@players[@current_player]} was sent to the penalty box"
-      @in_penalty_box[@current_player] = true
+      puts "#{name_of(current_player)} was sent to the penalty box"
+      @in_penalty_box[current_player] = true
 
       next_players_turn
       return true
@@ -133,23 +133,31 @@ module UglyTrivia
     private
 
     def did_player_win
-      @purses[@current_player] < 6
+      @purses[current_player] < 6
     end
 
     def next_players_turn
       @current_player += 1
-      @current_player = 0 if @current_player == how_many_players
+      @current_player = 0 if current_player == how_many_players
     end
 
     def award_gold_coin_to(player)
       @purses[player] += 1
-      puts "#{@players[player]} now has #{@purses[player]} Gold Coins."
+      puts "#{name_of(player)} now has #{@purses[player]} Gold Coins."
     end
 
     def move(player:, roll:)
       @places[player] = @places[player] + roll
       @places[player] = @places[player] - 12 if @places[player] > 11
-      puts "#{@players[player]}'s new location is #{@places[player]}"
+      puts "#{name_of(player)}'s new location is #{@places[player]}"
+    end
+
+    def is_in_penalty_box?(player)
+      @in_penalty_box[player]
+    end
+
+    def name_of(player)
+      @players[player]
     end
   end
 end
